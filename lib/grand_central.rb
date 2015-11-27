0bc0530eb7847138407880d350c0b35c25d544b3
Jamie Gaskins <jgaskins@gmail.com>
2015-11-27 15:23:12 -0500

    Don't require opal in Opal
    This could cause opal to be included in the payload, which causes
    problems if you're loading Opal from CDN instead of including it with
    your build.

--- lib/grand_central.rb
+++ lib/grand_central.rb
@@ -1,11 +1,11 @@
 require "grand_central/version"
 require "grand_central/store"
 require "grand_central/action"
-require "opal"
 
 module GrandCentral
 end
 
 if RUBY_ENGINE != 'opal'
+  require "opal"
   Opal.append_path File.expand_path('..', __FILE__)
 end
