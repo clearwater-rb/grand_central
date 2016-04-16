3bfd7d360d547155d642536d03302d7976aa85b2
Jamie Gaskins <jgaskins@gmail.com>
2016-04-16 01:58:04 -0400

    Add Model base class

---
+++ lib/grand_central/model.rb
@@ -0,0 +1,29 @@
+module GrandCentral
+  class Model
+    def self.attributes *attrs
+      @attributes ||= Set.new
+      if attrs.any?
+        @attributes += attrs
+        attr_reader *attrs
+      end
+
+      @attributes
+    end
+
+    def initialize attributes={}
+      self.class.attributes.each do |attr|
+        instance_variable_set "@#{attr}", attributes[attr]
+      end
+    end
+
+    def update attributes={}
+      self.class.new(to_h.merge(attributes))
+    end
+
+    def to_h
+      self.class.attributes.each_with_object({}) do |attr, hash|
+        hash[attr] = send(attr)
+      end
+    end
+  end
+end
